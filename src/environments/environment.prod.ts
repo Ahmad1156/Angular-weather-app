0483a0ac0d2de15815683f5330d445e7bd256f2f
ahmad shakik <ahmadshakik777@gmail.com>
2022-08-22 12:23:16 +0300

    test-deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl:'https://api.openweathermap.org/data/2.5/weather',
+
+  APPID:'c401bb5f059ccccaa9d79ffd9fa86686'
+
 };
